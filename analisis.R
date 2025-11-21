a5f3e4c1bba1eb6534e0b74aa5d590ca6d922f49
Diego Quijano <diegoalonsomq@gmail.com>
2025-11-21 14:33:22 -0600

    Incluyendo archivo ReadMe y Rmarkdown para reporte

--- analisis.R
+++ analisis.R
@@ -47,7 +47,7 @@ table(base_sc$exposicion_ocupacional,base_sc$grupo)
 chisq.test(table(base_sc$exposicion_ocupacional,base_sc$grupo))
 ## Frecuencias de exposicion ocupacional
 table(base_sc$producto_exposicion_ocupacional,base_sc$grupo)
-fisher.test(base_sc$producto_exposicion_ocupacional,base_sc$grupo)
+chisq.test(base_sc$producto_exposicion_ocupacional,base_sc$grupo)
 
 ## Saturacion de oxigeno
 shapiro.test(base_sc$saturacion_oxigeno)
@@ -55,51 +55,3 @@ base_sc|>filter(!is.na(saturacion_oxigeno) & !saturacion_oxigeno == 0)|>
   group_by(grupo)|>
   summarise(mediana = median(saturacion_oxigeno), rango_intercuartil = IQR(saturacion_oxigeno), minimo = min(saturacion_oxigeno),maximo = max(saturacion_oxigeno))
 wilcox.test(grupo_sxc$saturacion_oxigeno,grupo_ca$saturacion_oxigeno)
-
-## Genetica
-##drb_alelo1
-base_sc|>group_by(drb_alelo1,grupo)|>
-  summarise(n())|>
-  print(n=Inf)
-## drb_alelo2
-base_sc|>group_by(drb_alelo2,grupo)|>
-  summarise(n())|>
-  print(n=Inf)
-## dqb1_ alelo 1
-base_sc|>group_by(dqb1_alelo1,grupo)|>
-  summarise(n())|>
-  print(n=Inf)
-## dqb1_alelo2
-base_sc|>group_by(dqb1_alelo2,grupo)|>
-  summarise(n())|>
-  print(n=Inf)
-
-## Analisis exploratorio grafico
-
-## Indice tabaquico/ indice humo de leña / grupo
-base_sc|>ggplot(aes(iehl,indice_tabaquico, color = producto_exposicion_ocupacional))+
-  geom_point()+
-  scale_x_continuous(transform = "log10")+
-  scale_y_continuous(transform = "log10")+
-  facet_grid(.~grupo)
-
-## no fumadores / producto de exposicion
-base_sc|> filter(tabaquismo == "negado")|>
-  ggplot(aes(producto_exposicion_ocupacional,iehl,color = grupo, label=clave_muestra))+
-  geom_point()+
-  scale_y_continuous(transform = "log10")+
-  facet_grid(.~contacto_aves)
-
-base_sc|>ggplot(aes(grupo))+
-  geom_bar()+
-  facet_grid(tabaquismo~uso_de_leña)
-
-base_sc|>filter(tabaquismo %in% c("fumador","exfumador"))|>
-  ggplot(aes(grupo))+
-  geom_bar()
-
-base_sc|>filter(tipo_histologico %in% c(0,1))|>
-  ggplot(aes(indice_tabaquico))+
-  geom_histogram()+
-  facet_grid(tipo_histologico~.)
-table(base_sc$tipo_histologico,base_sc$indice_tabaquico)
